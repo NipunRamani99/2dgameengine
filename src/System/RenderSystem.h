2a45334e5e38eda4585525c773bae0f2202a774e
n1ps <ramaninipun@gmail.com>
2024-03-02 00:20:06 +0000

    implemented animation (#2)

--- src/System/RenderSystem.h
+++ src/System/RenderSystem.h
@@ -16,6 +16,7 @@ public:
 	{
 		RequireComponent<TransformComponent>();
 		RequireComponent<SpriteComponent>();
+		RequireComponent<AnimationComponent>();
 		Logger::Log("Initialized RenderSystem");
 
 	}
@@ -25,16 +26,22 @@ public:
 			for (Entity entity : GetEntities()) {
 
 				const TransformComponent& transform = registry->GetComponent<TransformComponent>(entity);
-				const SpriteComponent& sprite = registry->GetComponent<SpriteComponent>(entity);
+				SpriteComponent& sprite = registry->GetComponent<SpriteComponent>(entity);
+				const AnimationComponent& animData = registry->GetComponent<AnimationComponent>(entity);
+
 				if (sprite.zIndex == i) {
 					SDL_Texture* texture = sprite.texture;
-					const SDL_Rect& srcRect = sprite.srcRect;
+					
+					SDL_Rect& srcRect = sprite.srcRect;
+
+					srcRect.x = 32 * (animData.currentFrame - 1);
+
 					SDL_Rect destRect;
 					destRect.x = transform.position.x;
 					destRect.y = transform.position.y;
 					destRect.w = transform.scale.x * srcRect.w;
 					destRect.h = transform.scale.y * srcRect.h;
-
+					 
 					SDL_RenderCopyEx(renderer,
 						texture,
 						&srcRect,
@@ -43,7 +50,7 @@ public:
 						NULL,
 						SDL_FLIP_NONE);
 				}
-			}
+			} 
 		}
 	}
 
